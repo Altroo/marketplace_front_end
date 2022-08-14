762b1aa9e01d41a801643ce082d8b2944ace9a8d
Youness imac <y.elalami@qaryb.com>
2022-08-14 21:53:36 +0100

    fast-forward

--- components/htmlElements/buttons/chipButton/chipButton.tsx
+++ components/htmlElements/buttons/chipButton/chipButton.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import Styles from './chipButton.module.sass';
 import Chip from '@mui/material/Chip';
 import { chipActionsType, chipVariantType } from '../../../../types/ui/uiTypes';
+import { createTheme, ThemeProvider } from '@mui/material';
+import { hexToRGB } from "../../../../utils/helpers";
 
 type Props = {
 	actions: chipActionsType;
@@ -28,17 +30,37 @@ const ChipButtons: React.FC<Props> = (props: Props) => {
 				if (action.selected && action.border) {
 					cssStyle = { border: action.border, ...cssStyle };
 				}
+				let rippleColor = '#0D070B';
+				if (action.backgroundColor) {
+					if (action.backgroundColor !== '#FFFFFF') {
+						rippleColor = hexToRGB(action.backgroundColor, 1);
+					} else {
+						rippleColor = hexToRGB(rippleColor, 1);
+					}
+				}
+				const colorTheme = createTheme({
+					components: {
+						MuiChip: {
+							styleOverrides: {
+								root: {
+									color: rippleColor
+								},
+							},
+						},
+					},
+				});
 
 				return (
-					<Chip
-						key={index}
-						style={{ ...cssStyle }}
-						label={action.buttonText}
-						disabled={action.disabled}
-						variant={variant}
-						onClick={action.onClick}
-						className={Styles.chip}
-					/>
+					<ThemeProvider theme={colorTheme} key={index}>
+						<Chip
+							style={{ ...cssStyle }}
+							label={action.buttonText}
+							disabled={action.disabled}
+							variant={variant}
+							onClick={action.onClick}
+							className={Styles.chip}
+						/>
+					</ThemeProvider>
 				);
 			})}
 		</>
