d74d3dbba242c734bbc276beeb62f19ec096b9d2
Youness imac <y.elalami@qaryb.com>
2022-08-26 21:40:04 +0100

    test: added sass output style as expanded

--- next.config.js
+++ next.config.js
@@ -6,7 +6,8 @@ const path = require('path');
 const nextConfig = {
 	reactStrictMode: false,
 	sassOptions: {
-		includePaths: [path.join(__dirname, 'styles')]
+		includePaths: [path.join(__dirname, 'styles')],
+		outputStyle: 'expanded'
 	},
 	images: {
 		domains: [`${process.env.LOCAL_API_ROOT_URL}`]
