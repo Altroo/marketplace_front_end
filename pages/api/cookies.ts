2ff525c003e4c085fd583176fda7d3e4eb75e3fd
Youness imac <y.elalami@qaryb.com>
2022-08-26 20:50:34 +0100

    revert cookie changes secure to true

--- pages/api/cookies.ts
+++ pages/api/cookies.ts
@@ -5,7 +5,7 @@ export default function handler(req: NextApiRequest, res: NextApiResponse) {
 	if (req.method === 'POST') {
 		const options = {
 			httpOnly: true,
-			secure: false,
+			secure: true,
 			path: '/',
 			domain: `${process.env.NEXT_BACKEND_DOMAIN}`,
 		};
@@ -97,7 +97,7 @@ export default function handler(req: NextApiRequest, res: NextApiResponse) {
 			maxAge: -1,
 			path: '/',
 			httpOnly: true,
-			secure: false,
+			secure: true,
 			domain: `${process.env.NEXT_BACKEND_DOMAIN}`,
 		};
 		const query = req.body;
