bedad670fef4942d9d6ab5f582b4f0038e78173e
Youness imac <y.elalami@qaryb.com>
2022-08-30 15:03:33 +0100

    fix: changed cookie config sameSite to none & secure to true

--- pages/api/cookies.ts
+++ pages/api/cookies.ts
@@ -5,7 +5,7 @@ export default function handler(req: NextApiRequest, res: NextApiResponse) {
 	if (req.method === 'POST') {
 		const options = {
 			httpOnly: true,
-			secure: false,
+			secure: true,
 			path: '/',
 			domain: `${process.env.NEXT_BACKEND_DOMAIN}`,
 		};
@@ -14,7 +14,7 @@ export default function handler(req: NextApiRequest, res: NextApiResponse) {
 		if ('shop_name' in query) {
 			setCookie(res, '@shop_name', query.shop_name, {
 				maxAge: query.maxAge,
-				sameSite: 'lax',
+				sameSite: 'none',
 				...options,
 			});
 		}
@@ -22,7 +22,7 @@ export default function handler(req: NextApiRequest, res: NextApiResponse) {
 		if ('avatar' in query) {
 			setCookie(res, '@avatar', query.avatar, {
 				maxAge: query.maxAge,
-				sameSite: 'lax',
+				sameSite: 'none',
 				...options,
 			});
 		}
@@ -37,7 +37,7 @@ export default function handler(req: NextApiRequest, res: NextApiResponse) {
 		if ('color_code' in query) {
 			setCookie(res, '@color_code', query.color_code, {
 				maxAge: query.maxAge,
-				sameSite: 'lax',
+				sameSite: 'none',
 				...options,
 			});
 		}
@@ -46,42 +46,42 @@ export default function handler(req: NextApiRequest, res: NextApiResponse) {
 		if ('font_name' in query) {
 			setCookie(res, '@font_name', query.font_name, {
 				maxAge: query.maxAge,
-				sameSite: 'lax',
+				sameSite: 'none',
 				...options,
 			});
 		}
 		if ('tokenType' in query) {
 			setCookie(res, '@tokenType', query.tokenType, {
 				maxAge: query.maxAge,
-				sameSite: 'lax',
+				sameSite: 'none',
 				...options,
 			});
 		}
 		if ('initStateToken' in query) {
 			setCookie(res, '@initStateToken', query.initStateToken, {
 				maxAge: query.maxAge,
-				sameSite: 'lax',
+				sameSite: 'none',
 				...options,
 			});
 		}
 		if ('initStateUniqueID' in query) {
 			setCookie(res, '@initStateUniqueID', query.initStateUniqueID, {
 				maxAge: query.maxAge,
-				sameSite: 'lax',
+				sameSite: 'none',
 				...options,
 			});
 		}
 		if ('border' in query) {
 			setCookie(res, '@border', query.border, {
 				maxAge: query.maxAge,
-				sameSite: 'lax',
+				sameSite: 'none',
 				...options,
 			});
 		}
 		if ('icon_color' in query) {
 			setCookie(res, '@icon_color', query.icon_color, {
 				maxAge: query.maxAge,
-				sameSite: 'lax',
+				sameSite: 'none',
 				...options,
 			});
 		}
@@ -97,49 +97,49 @@ export default function handler(req: NextApiRequest, res: NextApiResponse) {
 			maxAge: -1,
 			path: '/',
 			httpOnly: true,
-			secure: false,
+			secure: true,
 			domain: `${process.env.NEXT_BACKEND_DOMAIN}`,
 		};
 		const query = req.body;
 		if ('shop_name' in query) {
 			deleteCookie(res, '@shop_name', {
-				sameSite: 'lax',
+				sameSite: 'none',
 				...options,
 			});
 		}
 		if ('avatar' in query) {
 			deleteCookie(res, '@avatar', {
-				sameSite: 'lax',
+				sameSite: 'none',
 				...options,
 			});
 		}
 		if ('color_code' in query) {
 			deleteCookie(res, '@color_code', {
-				sameSite: 'lax',
+				sameSite: 'none',
 				...options,
 			});
 		}
 		if ('font_name' in query) {
 			deleteCookie(res, '@font_name', {
-				sameSite: 'lax',
+				sameSite: 'none',
 				...options,
 			});
 		}
 		if ('tokenType' in query) {
 			deleteCookie(res, '@tokenType', {
-				sameSite: 'lax',
+				sameSite: 'none',
 				...options,
 			});
 		}
 		if ('initStateToken' in query) {
 			deleteCookie(res, '@initStateToken', {
-				sameSite: 'lax',
+				sameSite: 'none',
 				...options,
 			});
 		}
 		if ('initStateUniqueID' in query) {
 			deleteCookie(res, '@initStateUniqueID', {
-				sameSite: 'lax',
+				sameSite: 'none',
 				...options,
 			});
 		}
