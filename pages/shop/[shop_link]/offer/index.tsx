bcd0b9972f761de5f5776527dec870370d8cd1fb
Youness imac <y.elalami@qaryb.com>
2022-10-06 17:40:29 +0100

    fast-forward

--- pages/shop/[shop_link]/offer/index.tsx
+++ pages/shop/[shop_link]/offer/index.tsx
@@ -6,15 +6,22 @@ import SellProductIcon from '../../../../public/assets/svgs/globalIcons/sell-pro
 import SellServiceIcon from '../../../../public/assets/svgs/globalIcons/sell-service-icon.svg';
 import SellLocationIcon from '../../../../public/assets/svgs/globalIcons/sell-location-icon.svg';
 import { Stack } from '@mui/material';
-import { REAL_OFFER_ADD_PRODUCT_CATEGORIES, TEMP_SHOP_ADD_SHOP_NAME } from "../../../../utils/routes";
+import {
+	REAL_OFFER_ADD_PRODUCT_CATEGORIES,
+	REAL_OFFER_ADD_SERVICE_CATEGORIES,
+	TEMP_SHOP_ADD_SHOP_NAME
+} from "../../../../utils/routes";
 import { getCookie } from "cookies-next";
 import { useRouter } from "next/router";
+import UserMainNavigationBar from "../../../../components/layouts/userMainNavigationBar/userMainNavigationBar";
 
 const Index: NextPage = () => {
 	const router = useRouter();
 
 	return (
-		<main className={Styles.main}>
+		<Stack direction="column">
+			<UserMainNavigationBar />
+			<main className={Styles.main}>
 			<Stack direction="row" justifyContent="space-evenly" alignItems="center" spacing={3} className={Styles.flexWrapper}>
 				<h2 className={Styles.header}>Choississez le type d&apos;offre que vous souhaitez vendre</h2>
 				<Stack direction="column" spacing={2} className={Styles.cardsWrapper}>
@@ -28,7 +35,7 @@ const Index: NextPage = () => {
 						title="Offrir un service"
 						svgIcon={SellServiceIcon}
 						description="de cours, de transport, de réparation…"
-						nextPage="#"
+						nextPage={REAL_OFFER_ADD_SERVICE_CATEGORIES(router.query.shop_link as string)}
 					/>
 					<OfferTypeCard
 						title="Location"
@@ -38,6 +45,7 @@ const Index: NextPage = () => {
 				</Stack>
 			</Stack>
 		</main>
+		</Stack>
 	);
 };
 
--- pages/temp-offer/index.tsx
+++ pages/temp-offer/index.tsx
@@ -6,35 +6,43 @@ import SellProductIcon from '../../public/assets/svgs/globalIcons/sell-product-i
 import SellServiceIcon from '../../public/assets/svgs/globalIcons/sell-service-icon.svg';
 import SellLocationIcon from '../../public/assets/svgs/globalIcons/sell-location-icon.svg';
 import { Stack } from '@mui/material';
-import { TEMP_OFFER_ADD_PRODUCT_CATEGORIES, TEMP_SHOP_ADD_SHOP_NAME } from "../../utils/routes";
+import {
+	TEMP_OFFER_ADD_PRODUCT_CATEGORIES,
+	TEMP_OFFER_ADD_SERVICE_CATEGORIES,
+	TEMP_SHOP_ADD_SHOP_NAME
+} from "../../utils/routes";
 import { getCookie } from "cookies-next";
+import UserMainNavigationBar from "../../components/layouts/userMainNavigationBar/userMainNavigationBar";
 
 const Index: NextPage = () => {
 	return (
-		<main className={Styles.main}>
-			<Stack direction="row" justifyContent="space-evenly" alignItems="center" spacing={3} className={Styles.flexWrapper}>
-				<h2 className={Styles.header}>Choississez le type d&apos;offre que vous souhaitez vendre</h2>
-				<Stack direction="column" spacing={2} className={Styles.cardsWrapper}>
-					<OfferTypeCard
-						title="Vendre"
-						svgIcon={SellProductIcon}
-						description="un produit, un bien immobilier, un véhicule…"
-						nextPage={TEMP_OFFER_ADD_PRODUCT_CATEGORIES}
-					/>
-					<OfferTypeCard
-						title="Offrir un service"
-						svgIcon={SellServiceIcon}
-						description="de cours, de transport, de réparation…"
-						nextPage="#"
-					/>
-					<OfferTypeCard
-						title="Location"
-						svgIcon={SellLocationIcon}
-						description="un produit, un bien immobilier, une salle des fêtes, un véhicule…"
-					/>
+		<Stack direction="column">
+			<UserMainNavigationBar />
+			<main className={Styles.main}>
+				<Stack direction="row" justifyContent="space-evenly" alignItems="center" spacing={3} className={Styles.flexWrapper}>
+					<h2 className={Styles.header}>Choississez le type d&apos;offre que vous souhaitez vendre</h2>
+					<Stack direction="column" spacing={2} className={Styles.cardsWrapper}>
+						<OfferTypeCard
+							title="Vendre"
+							svgIcon={SellProductIcon}
+							description="un produit, un bien immobilier, un véhicule…"
+							nextPage={TEMP_OFFER_ADD_PRODUCT_CATEGORIES}
+						/>
+						<OfferTypeCard
+							title="Offrir un service"
+							svgIcon={SellServiceIcon}
+							description="de cours, de transport, de réparation…"
+							nextPage={TEMP_OFFER_ADD_SERVICE_CATEGORIES}
+						/>
+						<OfferTypeCard
+							title="Location"
+							svgIcon={SellLocationIcon}
+							description="un produit, un bien immobilier, une salle des fêtes, un véhicule…"
+						/>
+					</Stack>
 				</Stack>
-			</Stack>
-		</main>
+			</main>
+		</Stack>
 	);
 };
 
