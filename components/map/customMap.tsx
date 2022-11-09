229e6598cb8df63d8550be49752b12ed74afac59
Yooy <scorpion.yooy@gmail.com>
2022-11-09 13:13:39 +0100

    fix: map - 2

--- components/map/customMap.tsx
+++ components/map/customMap.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useCallback } from 'react';
 import Styles from './customMap.module.sass';
 import { useAppDispatch } from '../../utils/hooks';
 import { MapContainer, TileLayer, Marker, useMapEvents } from 'react-leaflet';
@@ -7,6 +7,30 @@ import { placesGetGeolocalisationAction } from '../../store/actions/places/place
 import { MarkerProps } from 'react-leaflet/lib/Marker';
 import { ShopZoneByType } from '../../types/shop/shopTypes';
 import LocalisationNamePopup from './localisationNamePopup/localisationNamePopup';
+
+const adresseSVG =
+	'<svg xmlns="http://www.w3.org/2000/svg" width="46" height="56"><path fill-rule="evenodd" d="M39.263 7.673c8.897 8.812 8.966 23.168.153 32.065l-.153.153L23 56 6.737 39.89C-2.16 31.079-2.23 16.723 6.584 7.826l.153-.152c9.007-8.922 23.52-8.922 32.526 0zM23 14.435c-5.211 0-9.436 4.185-9.436 9.347S17.79 33.128 23 33.128s9.436-4.184 9.436-9.346S28.21 14.435 23 14.435z"/></svg>';
+const adresseSVGURL = 'data:image/svg+xml;base64,' + Buffer.from(adresseSVG).toString('base64');
+
+const secteurSVG =
+		'<svg width="394" height="394" viewBox="0 0 394 394" fill="none" xmlns="http://www.w3.org/2000/svg"><circle opacity="0.11" cx="197" cy="197" r="197" fill="#0274D7"/><circle cx="197" cy="197" r="11" fill="#0D070B"/></svg>';
+const secteurSVGURL = 'data:image/svg+xml;base64,' + Buffer.from(secteurSVG).toString('base64');
+
+const adresseIcon = new L.Icon({
+	iconUrl: adresseSVGURL,
+	iconRetinaUrl: adresseSVGURL,
+	iconSize: [46, 56],
+	iconAnchor: [18, 50],
+	popupAnchor: [0, -32],
+});
+
+const secteurIcon = new L.Icon({
+	iconUrl: secteurSVGURL,
+	iconRetinaUrl: secteurSVGURL,
+	iconSize: [300, 300],
+	iconAnchor: [300 / 2, 300 / 2],
+	popupAnchor: [0, 0],
+});
 
 export type PositionType = { lat: number; lng: number };
 type Props = {
@@ -24,39 +48,15 @@ const CustomMap: React.FC<Props> = (props: Props) => {
 	const markerRef = useRef<L.Marker>(null);
 	const mapRef = useRef<L.Map>(null);
 
-	const adresseSVG =
-		'<svg xmlns="http://www.w3.org/2000/svg" width="46" height="56"><path fill-rule="evenodd" d="M39.263 7.673c8.897 8.812 8.966 23.168.153 32.065l-.153.153L23 56 6.737 39.89C-2.16 31.079-2.23 16.723 6.584 7.826l.153-.152c9.007-8.922 23.52-8.922 32.526 0zM23 14.435c-5.211 0-9.436 4.185-9.436 9.347S17.79 33.128 23 33.128s9.436-4.184 9.436-9.346S28.21 14.435 23 14.435z"/></svg>';
-	const adresseSVGURL = 'data:image/svg+xml;base64,' + Buffer.from(adresseSVG).toString('base64');
-
-	const adresseIcon = new L.Icon({
-		iconUrl: adresseSVGURL,
-		iconRetinaUrl: adresseSVGURL,
-		iconSize: [46, 56],
-		iconAnchor: [18, 50],
-		popupAnchor: [0, -32],
-	});
-
-	const secteurSVG =
-		'<svg width="394" height="394" viewBox="0 0 394 394" fill="none" xmlns="http://www.w3.org/2000/svg"><circle opacity="0.11" cx="197" cy="197" r="197" fill="#0274D7"/><circle cx="197" cy="197" r="11" fill="#0D070B"/></svg>';
-
-	const secteurSVGURL = 'data:image/svg+xml;base64,' + Buffer.from(secteurSVG).toString('base64');
-	const secteurIcon = new L.Icon({
-		iconUrl: secteurSVGURL,
-		iconRetinaUrl: secteurSVGURL,
-		iconSize: [300, 300],
-		iconAnchor: [300 / 2, 300 / 2],
-		popupAnchor: [0, 0],
-	});
-
-	const AdresseMarker = (props: MarkerProps) => {
+	const AdresseMarker = useCallback((props: MarkerProps) => {
 		const position = props.position as [number, number];
 		return <Marker autoPan={true} draggable={false} position={position} ref={markerRef} icon={props.icon}></Marker>;
-	};
+	}, []);
 
-	const SecteurMarker = (props: MarkerProps) => {
+	const SecteurMarker = useCallback((props: MarkerProps) => {
 		const position = props.position as [number, number];
 		return <Marker autoPan={true} draggable={false} position={position} ref={markerRef} icon={props.icon}></Marker>;
-	};
+	}, []);
 
 	const CustomMapEvents = () => {
 		const marker = markerRef.current;
