f2f2ff936e991a9dc2042c6528091d82d4dff190
Yooy <scorpion.yooy@gmail.com>
2022-11-23 18:03:09 +0100

    added sentry setup

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import("next").NextConfig} */
 
 const path = require('path');
+const { withSentryConfig } = require('@sentry/nextjs')
 
 const nextConfig = {
 	reactStrictMode: false,
@@ -15,10 +16,27 @@ const nextConfig = {
         hostname: `${process.env.API_ROOT_URL}`
       }
 		]
-	}
+	},
+	sentry: {
+    // Use `hidden-source-map` rather than `source-map` as the Webpack `devtool`
+    // for client-side builds. (This will be the default starting in
+    // `@sentry/nextjs` version 8.0.0.) See
+    // https://webpack.js.org/configuration/devtool/ and
+    // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/#use-hidden-source-map
+    // for more information.
+    hideSourceMaps: true
+  }
 };
 
-module.exports = nextConfig;
+module.exports = withSentryConfig(nextConfig, {
+	// Additional config options for the Sentry Webpack plugin. Keep in mind that
+  // the following options are set automatically, and overriding them is not
+  // recommended:
+  //   release, url, org, project, authToken, configFile, stripPrefix,
+  //   urlPrefix, include, ignore
+  // For all available options, see:
+  // https://github.com/getsentry/sentry-webpack-plugin#options.
+});
 
 // module.exports = {
 // 	...nextConfig,
