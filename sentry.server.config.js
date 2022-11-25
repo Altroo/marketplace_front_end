f42ea95e5cde0765bbbff3df2c415f2c5a6623eb
Yooy <scorpion.yooy@gmail.com>
2022-11-25 13:54:07 +0100

    fix: sentry setups - 5

--- sentry.server.config.js
+++ sentry.server.config.js
@@ -9,7 +9,7 @@ import { BrowserTracing } from '@sentry/tracing';
 const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
 
 Sentry.init({
-  environment: "production",
+  // environment: "production",
   dsn: SENTRY_DSN,
   tracesSampleRate: 1.0,
   integrations: [
