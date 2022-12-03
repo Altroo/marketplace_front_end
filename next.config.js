56a5716981249bd4601391587a97e3d64a963370
Yooy <scorpion.yooy@gmail.com>
2022-12-03 15:07:03 +0100

    added rewrite manually

--- next.config.js
+++ next.config.js
@@ -1,42 +1,50 @@
 /** @type {import("next").NextConfig} */
 
 const path = require('path');
-const { withSentryConfig } = require('@sentry/nextjs')
+const { withSentryConfig } = require('@sentry/nextjs');
 
 const nextConfig = {
 	reactStrictMode: false,
 	sassOptions: {
-		includePaths: [path.join(__dirname, 'styles')]
+		includePaths: [path.join(__dirname, 'styles')],
 	},
 	images: {
 		domains: [`${process.env.API_ROOT_URL}`],
 		remotePatterns: [
 			{
-        protocol: `${process.env.HTTP_PROTOCOLE}`,
-        hostname: `${process.env.API_ROOT_URL}`
-      }
-		]
+				protocol: `${process.env.HTTP_PROTOCOLE}`,
+				hostname: `${process.env.API_ROOT_URL}`,
+			},
+		],
 	},
 	sentry: {
-    // Use `hidden-source-map` rather than `source-map` as the Webpack `devtool`
-    // for client-side builds. (This will be the default starting in
-    // `@sentry/nextjs` version 8.0.0.) See
-    // https://webpack.js.org/configuration/devtool/ and
-    // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/#use-hidden-source-map
-    // for more information.
-    hideSourceMaps: true
-  }
+		// Use `hidden-source-map` rather than `source-map` as the Webpack `devtool`
+		// for client-side builds. (This will be the default starting in
+		// `@sentry/nextjs` version 8.0.0.) See
+		// https://webpack.js.org/configuration/devtool/ and
+		// https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/#use-hidden-source-map
+		// for more information.
+		hideSourceMaps: true,
+	},
+	async rewrites() {
+		return [
+			{
+				source: 'https://:qaryb.com/api/:path*',
+				destination: `https://:www.qaryb.com/api/:path*`,
+			},
+		];
+	},
 };
 
 module.exports = withSentryConfig(nextConfig, {
 	// Additional config options for the Sentry Webpack plugin. Keep in mind that
-  // the following options are set automatically, and overriding them is not
-  // recommended:
-  //   release, url, org, project, authToken, configFile, stripPrefix,
-  //   urlPrefix, include, ignore
-  // For all available options, see:
-  // https://github.com/getsentry/sentry-webpack-plugin#options.
-	silent: false
+	// the following options are set automatically, and overriding them is not
+	// recommended:
+	//   release, url, org, project, authToken, configFile, stripPrefix,
+	//   urlPrefix, include, ignore
+	// For all available options, see:
+	// https://github.com/getsentry/sentry-webpack-plugin#options.
+	silent: false,
 });
 
 // module.exports = {
