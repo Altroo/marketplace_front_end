0de5ce966c1019d3e5110d17f05f830427ee85a5
Yooy <scorpion.yooy@gmail.com>
2022-12-03 16:25:39 +0100

    added cors headers - test 6

--- pages/api/cookie/delete.ts
+++ pages/api/cookie/delete.ts
@@ -6,7 +6,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 	await NextCors(req, res, {
       // Options
       methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
-      origin: '*',
+      origin: ['https://www.qaryb.com', 'https://qaryb.com'],
       optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
    });
 
