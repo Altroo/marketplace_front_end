f9abc1b57c375c5247d62372de47d8b9ec94e9c7
Yooy <scorpion.yooy@gmail.com>
2023-01-25 11:24:48 +0100

    fixed front-end logs for good

--- next-logger.config.js
+++ next-logger.config.js
@@ -1,36 +1,15 @@
 // next-logger.config.js
-const pino = require('pino')
-// const levels = {
-//   http: 10,
-//   debug: 20,
-//   info: 30,
-//   warn: 40,
-//   error: 50,
-//   fatal: 60,
-// };
-module.exports = pino(
-  {
-    // customLevels: levels, // our defined levels
-    // useOnlyCustomLevels: true,
-    level: 'debug',
-    formatters: {
-      level: (label) => {
-        return { level: label };
-      },
-    },
-    name: 'next-log',
-    errorKey: 'Error',
-    messageKey: 'Info',
-    enabled: true,
-    timestamp: pino.stdTimeFunctions.isoTime,
-    transport: {
-      target: 'pino-pretty',
-      options: {
-        translateTime: 'SYS:standard',
-        colorize: true,
-        levelFirst: true,
-      },
-    },
-  },
-  pino.destination(`${__dirname}/debug/debug.log`)
-)
+const pino = require('pino');
+const logger = (defaultConfig) =>
+	pino({
+		...defaultConfig,
+		formatters: {
+			level: (label) => {
+				return { level: label };
+			},
+		},
+		timestamp: pino.stdTimeFunctions.isoTime,
+	}, pino.destination(`${__dirname}/logs/debug.log`));
+module.exports = {
+	logger,
+};
